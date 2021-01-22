126922d5733e409f4273caca1e221d9eb34de116
Nicolas Lachise <webdeveloppeurlyon@gmail.com>
2021-01-23 00:33:11 +0100

    Express app Suppression server.js npm cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const Airtable = require("airtable");
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
 
 // ENV VARIABLE
 const apiKey = process.env.AIRTABLE_API_KEY;
@@ -7,6 +9,9 @@ const baseId = process.env.AIRTABLE_BASE_ID;
 
 // Variable
 let listTournois = ["Sélectionner un tournois"];
+// User test pour auth
+const users = [{ id: '1', username: 'nijlak', password: '123' }]
+const SECRET = 'TOKEN'
 
 const express = require("express");
 
@@ -16,22 +21,42 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+app.use(cors())
 
 // Connect Airtable
 
 const base = new Airtable({ apiKey: apiKey }).base(baseId);
+
+// Middleware
+/* Récupération du header bearer */
+const extractBearerToken = headerValue => {
+  if (typeof headerValue !== 'string') {
+    return false
+  }
+
+  const matches = headerValue.match(/(bearer)\s+(\S+)/i)
+  return matches && matches[2]
+}
+
+/* Vérification du token */
+const checkTokenMiddleware = (req, res, next) => {
+  // Récupération du token
+  const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+
+  // Présence d'un token
+  if (!token) {
+    return res.status(401).json({ message: 'Error. Need a token' })
+  }
+
+  // Véracité du token
+  jwt.verify(token, SECRET, (err, decodedToken) => {
+    if (err) {
+      res.status(401).json({ message: 'Error. Bad token' })
+    } else {
+      return next()
+    }
+  })
+}
 
 // Logique
 
@@ -44,7 +69,7 @@ app.get("/allrecord", (req, res, next) => {
     .eachPage(
       function page(records, fetchNextPage) {
         records.forEach(function (record) {
-          list.push({ Name: record.get("Nom"), id: record.id });
+          list.push({ name: record.get("Nom"), id: record.id, start: record.get("Date_debut") });
         });
         fetchNextPage();
         res.status(200).json({ Retrieved: list });
@@ -66,4 +91,68 @@ app.get("/onerecord", (req, res, next) => {
   });
 });
 
-module.exports = app;
+/* Formulaire de connexion */
+app.post('/login', (req, res) => {
+  // Pas d'information à traiter
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Error. Please enter the correct username and password' })
+  }
+
+  // Checking
+  const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+
+  // Pas bon
+  if (!user) {
+    return res.status(400).json({ message: 'Error. Wrong login or password' })
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.username
+    },
+    SECRET,
+    { expiresIn: '24 hours' }
+  )
+
+  return res.json({ token: token })
+})
+
+app.get('/me', checkTokenMiddleware, (req, res) => {
+  // Récupération du token
+  const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+  // Décodage du token
+  const decoded = jwt.verify(token, SECRET, { complete: false })
+
+  return res.json({ user: decoded })
+})
+
+app.post('/register', (req, res) => {
+  // Aucune information à traiter
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Error. Please enter username and password' })
+  }
+
+  // Checking
+  const userExisting = users.find(u => u.username === req.body.username)
+
+  // Pas bon
+  if (userExisting) {
+    return res.status(400).json({ message: `Error. User ${req.body.username} already existing` })
+  }
+
+  // Données du nouvel utilisateur
+  const id = users[users.length - 1].id + 1
+  const newUser = {
+    id: id,
+    username: req.body.username,
+    password: req.body.password
+  }
+
+  // Insertion dans le tableau des utilisateurs
+  users.push(newUser)
+
+  return res.status(201).json({ message: `User ${id} created` })
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port " + process.env.PORT + "..."));
